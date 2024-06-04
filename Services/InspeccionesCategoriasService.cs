92d2b76330c6f338bfbdc97c09b53c59ff287a32
samhernandezc24 <samd.hernandezc24@gmail.com>
2024-06-03 22:50:27 -0600

    Update InspeccionesCategoriasService

--- Services/InspeccionesCategoriasService.cs
+++ Services/InspeccionesCategoriasService.cs
@@ -2,6 +2,7 @@
 using API.Inspecciones.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
 using System.Security.Claims;
 using Workcube.Libraries;
 
@@ -23,10 +24,16 @@ namespace API.Inspecciones.Services
             var objUser = Globals.GetUser(user);
             var objTransaction = _context.Database.BeginTransaction();
 
-            string idInspeccion     = Globals.ParseGuid(data.idInspeccion);
-            bool isParcial          = Globals.ParseBool(data.isParcial);
+            string idInspeccion = Globals.ParseGuid(data.idInspeccion);
+            bool isParcial      = Globals.ParseBool(data.isParcial);
 
+            // ACTUALIZAR VALORES DE LA INSPECCION
             Inspeccion objInspeccion = await _inspeccionesService.Find(idInspeccion);
+
+            if (objInspeccion == null) { throw new ArgumentException("No se encontró la inspección."); }
+
+            objInspeccion.IdInspeccionEstatus           = "ea52bdfd-8af6-4f5a-b182-2b99e554eb32";
+            objInspeccion.InspeccionEstatusName         = "EVALUACIÓN";
 
             objInspeccion.FechaInspeccionInicial        = Globals.DateTime(data.fechaInspeccionInicial);
             objInspeccion.FechaInspeccionInicialUpdate  = Globals.DateTime(data.fechaInspeccionInicial);
@@ -40,44 +47,72 @@ namespace API.Inspecciones.Services
 
             if (!isParcial) { objInspeccion.Evaluado = true; }
 
-            objInspeccion.FechaEvaluacion   = DateTime.Now;
+            objInspeccion.FechaEvaluacion = DateTime.Now;
             objInspeccion.SetUpdated(objUser);
 
-            List<InspeccionCategoria> rangeCategoria            = new List<InspeccionCategoria>();
-            List<InspeccionCategoriaValue> rangeCategoriaValue  = new List<InspeccionCategoriaValue>();
+            var lstCategorias = _context.InspeccionesCategorias.Where(x => x.IdInspeccion == idInspeccion).ToList();
+            List<string> idsCategorias = lstCategorias.Select(x => x.IdInspeccionCategoria).ToList();
+            var lstCategoriasValues = _context.InspeccionesCategoriasValues.Where(x => idsCategorias.Contains(x.IdInspeccionCategoria)).ToList();
 
             foreach (var categoria in data.categorias)
             {
-                InspeccionCategoria objCategoria = new InspeccionCategoria();
+                string idCategoria = Globals.ParseGuid(categoria.idCategoria);
+                InspeccionCategoria objCategoria = lstCategorias.FirstOrDefault(x => x.IdCategoria == idCategoria);
 
-                objCategoria.IdInspeccionCategoria  = Guid.NewGuid().ToString();
-                objCategoria.IdCategoria            = Globals.ParseGuid(categoria.idCategoria);
-                objCategoria.CategoriaName          = Globals.ToUpper(categoria.name);
-                objCategoria.IdInspeccion           = idInspeccion;
-                objCategoria.SetCreated(objUser);
-                rangeCategoria.Add(objCategoria);
+                if (objCategoria == null)
+                {
+                    // GUARDAR INSPECCION CATEGORIA
+                    objCategoria = new InspeccionCategoria();
+
+                    objCategoria.IdInspeccionCategoria  = Guid.NewGuid().ToString();
+                    objCategoria.IdCategoria            = Globals.ParseGuid(categoria.idCategoria);
+                    objCategoria.CategoriaName          = Globals.ToUpper(categoria.name);
+                    objCategoria.IdInspeccion           = idInspeccion;
+                    objCategoria.SetCreated(objUser);
+
+                    _context.InspeccionesCategorias.Add(objCategoria);
+                }              
 
                 foreach (var item in categoria.categoriasItems)
                 {
-                    InspeccionCategoriaValue objValue = new InspeccionCategoriaValue();
+                    string idCategoriaItem = Globals.ParseGuid(item.idCategoriaItem);
+                    InspeccionCategoriaValue objValue = lstCategoriasValues.FirstOrDefault(x => x.IdCategoriaItem == idCategoriaItem);
 
-                    objValue.IdInspeccionCategoriaValue     = Guid.NewGuid().ToString();
-                    objValue.IdInspeccionCategoria          = objCategoria.IdInspeccionCategoria;
-                    objValue.IdCategoriaItem                = Globals.ParseGuid(item.idCategoriaItem);
-                    objValue.CategoriaItemName              = Globals.ToUpper(item.name);
-                    objValue.IdFormularioTipo               = Globals.ParseGuid(item.idFormularioTipo);
-                    objValue.FormularioTipoName             = Globals.ToUpper(item.formularioTipoName);
-                    objValue.FormularioValor                = Globals.ToString(item.formularioValor);
-                    objValue.Value                          = Globals.ToString(item.value);
-                    objValue.NoAplica                       = Globals.ParseBool(item.noAplica);
-                    objValue.Observaciones                  = Globals.ToString(item.observaciones);
-                    objValue.SetCreated(objUser);
-                    rangeCategoriaValue.Add(objValue);
+                    if (objValue != null)
+                    {
+                        // ACTUALIZAR INSPECCION CATEGORIA VALUE
+                        objValue.CategoriaItemName  = Globals.ToUpper(item.name);
+                        objValue.IdFormularioTipo   = Globals.ParseGuid(item.idFormularioTipo);
+                        objValue.FormularioTipoName = Globals.ToUpper(item.formularioTipoName);
+                        objValue.FormularioValor    = Globals.ToString(item.formularioValor);
+                        objValue.Value              = Globals.ToString(item.value);
+                        objValue.NoAplica           = Globals.ParseBool(item.noAplica);
+                        objValue.Observaciones      = Globals.ToUpper(item.observaciones);
+                        objValue.SetUpdated(objUser);
+
+                        _context.InspeccionesCategoriasValues.Update(objValue);
+                    } 
+                    else
+                    {
+                        // GUARDAR INSPECCION CATEGORIA VALUE
+                        objValue = new InspeccionCategoriaValue();
+
+                        objValue.IdInspeccionCategoriaValue     = Guid.NewGuid().ToString();
+                        objValue.IdInspeccionCategoria          = objCategoria.IdInspeccionCategoria;
+                        objValue.IdCategoriaItem                = Globals.ParseGuid(item.idCategoriaItem);
+                        objValue.CategoriaItemName              = Globals.ToUpper(item.name);
+                        objValue.IdFormularioTipo               = Globals.ParseGuid(item.idFormularioTipo);
+                        objValue.FormularioTipoName             = Globals.ToUpper(item.formularioTipoName);
+                        objValue.FormularioValor                = Globals.ToString(item.formularioValor);
+                        objValue.Value                          = Globals.ToString(item.value);
+                        objValue.NoAplica                       = Globals.ParseBool(item.noAplica);
+                        objValue.Observaciones                  = Globals.ToUpper(item.observaciones);
+                        objValue.SetCreated(objUser);
+
+                        _context.InspeccionesCategoriasValues.Add(objValue);
+                    }
                 }
             }
-
-            _context.InspeccionesCategorias.AddRange(rangeCategoria);
-            _context.InspeccionesCategoriasValues.AddRange(rangeCategoriaValue);
 
             await _context.SaveChangesAsync();
             objTransaction.Commit();
